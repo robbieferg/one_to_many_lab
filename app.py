572cd6442adb0b43759aa2ce558174ef620f6418
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-01 14:52:42 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from controllers.library_controller import library_blueprint
+
+app = Flask(__name__)
+
+app.register_blueprint(library_blueprint)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
