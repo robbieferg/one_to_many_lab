726753d617a43938fe982ed82e352e672dd30c94
Robbie Ferguson <robbiejferguson92@gmail.com>
2021-12-01 15:04:53 +0000

    tables added

---
+++ db/library.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255)
+);
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    genre VARCHAR(255),
+    page_count INT,
+    completed BOOLEAN,
+    author_id INT REFERENCES authors(id)
+);
